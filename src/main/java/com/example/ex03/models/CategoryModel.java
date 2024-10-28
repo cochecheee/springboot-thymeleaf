98d57f49a058fd33b224d85eb6de7248c855ee84
tien.blt <buitien747@gmail.com>
2024-10-28 16:08:00 +0700

    initialize models

---
+++ src/main/java/com/example/ex03/models/CategoryModel.java
@@ -0,0 +1,32 @@
+package com.example.ex03.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryModel implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	
+	private String name;
+	
+	private String images;
+	
+	private int status;
+	
+	private Boolean isEdit;
+}
